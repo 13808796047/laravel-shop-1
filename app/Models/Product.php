38c4ce3fe855010b681862259410a15a0a0d9862
Darker <511391805@qq.com>
2018-07-31 01:02:57 +0800

    用户界面-商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -18,5 +20,15 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    //获取图片路径
+    public function getImageUrlAttribute()
+    {
+        //如果image字段本身就已经是完整url就直接返回
+        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+            return $this->attributes['image'];
+        }
+        return Storage::disk('public')->url($this->attributes['image']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', 'PagesController@index')->name('index');
+Route::redirect('/', '/products')->name('index');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 
 Auth::routes();
@@ -22,10 +23,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     //开始
     Route::group(['middleware' => 'email_verified'], function () {
-//        Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
-//        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-//        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-//        Route::get('user_addresses/{address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::resource('user_addresses','UserAddressesController');
     });
     //结束
